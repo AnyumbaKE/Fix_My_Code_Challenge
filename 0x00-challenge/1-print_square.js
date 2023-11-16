ee965c814ecf92a572c483cb6136d3f3d7428878
AnyumbaKE <esanyumba@gmail.com>
2023-11-16 09:46:31 -0800

    1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -11,7 +11,7 @@ if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
 size = parseInt(process.argv[2], 10);
