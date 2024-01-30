692d22f58b9f6eab05f5dcfdbb26cae78bb49b69
AnyumbaKE <esanyumba@gmail.com>
2024-01-31 01:36:12 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+'''
+Module suare
+contains class square
+'''
 
-class square():
 
+class Square():
+    '''
+    Class square
+    '''
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        '''Class Init method'''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +22,17 @@ class square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
+        '''Perimeter of the square'''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        '''Class handle print'''
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    '''Test Class'''
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
